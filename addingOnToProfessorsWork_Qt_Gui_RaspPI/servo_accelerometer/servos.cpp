a304dcced26a22129ac5f64c4ab60c85c2b1de68
Muhammad Ayub <tuf22191@Temple.edu>
2016-04-05 11:36:57 +0000

    Servo is working both directions! For pin 18, that is

--- addingOnToProfessorsWork_Qt_Gui_RaspPI/servo_accelerometer/servos.cpp
+++ addingOnToProfessorsWork_Qt_Gui_RaspPI/servo_accelerometer/servos.cpp
@@ -1,28 +1,50 @@
 //this is a servos file
 
-#include <wiringPi.h>
-#include <softPwm.h>
+
+
+#include <stdio.h>
+#include <bcm2835.h>
+//#include <wiringPi.h>
+//#include <softPwm.h>
 #include <cstdio>
+
+
+//taken from online  From Airspayce.com written by Mike McCauley
+#define PIN RPI_GPIO_P1_12
 
 
 int main() {
 
+
+if(!bcm2835_init()){return 1;}
+
+bcm2835_gpio_write(PIN,HIGH);
+
+
 int pin7 = 7;
-int pinx= 2;
+int pinx= 18;
 int result =-1;
 
-result=softPwmCreate(7,15,215);
+
+
+result=0;
+//result=softPwmCreate(pinx,0,215);
 if(result ==0){
 
-while(1){
+while(result<1000){
 
-printf("In while loop");
+bcm2835_gpio_write(PIN,HIGH);
+bcm2835_delay(3);
+bcm2835_gpio_write(PIN,LOW);
+bcm2835_delay(20);
 
+//printf("In while loop");
+result++;
 }
 
 }
 
-
+bcm2835_close();
 
 
 return 0; 
